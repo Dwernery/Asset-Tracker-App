df9d1b0f4c27a8765128d0e30ad62b177e0c401d
David <david.wernery@gmail.com>
2022-05-06 17:04:33 -0400

    c

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const Asset = require('./models/assetModel.js')
 
 const DBConnection = async () => {
   const connection = await mongoose.connect("mongodb://assettrackerappdb:Io670STskFkM1FqypzUPoLNKpEhT27qw8nH7Tg6BNwLvufzK3uUsdQMaLuu5S2KlAbjuSoiK8QCW7j6rQMTgYA==@assettrackerappdb.mongo.cosmos.azure.com:10255/asset-tracker?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@assettrackerappdb@", {
-    useNewUrlParser: true,
+    useNewUrlParser: true
   })
   console.log(connection, 'Connected to Database')
 }
